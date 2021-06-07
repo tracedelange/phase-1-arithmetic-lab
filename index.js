774dddb5c6203e3f6cc1f7f82135ef3691ba3f91
Trace <tracedelange@me.com>
2021-06-07 11:42:18 -0500

    commit tree 57d4b776a9ae4d347ecb9728295611f69647347c

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = (Math.floor(Math.random()*10))+1
+
+const num3 = 44
+const num4 = 10
+
+const mod = num3 % num4
+
+const nums = (1,2,3,20)
+
+const max = Math.max(nums)
